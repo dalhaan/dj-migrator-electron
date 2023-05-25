7107c07fd69771ef8052523f6531a77f229ac827
Dallan Freemantle <dallan@alphero.com>
2023-05-25 23:04:06 +1200

    Add crates to import store

--- apps/electron-app/src/renderer/import-window/stores/import-store.ts
+++ apps/electron-app/src/renderer/import-window/stores/import-store.ts
@@ -3,9 +3,11 @@ import { create } from "zustand";
 export const useImport = create<{
   crates: string[];
   selectedCrates: string[];
+  setCrates: (crates: string[]) => void;
   setSelectedCrates: (crates: string[]) => void;
 }>((set) => ({
   crates: [],
   selectedCrates: [],
+  setCrates: (crates: string[]) => set({ crates }),
   setSelectedCrates: (crates: string[]) => set({ selectedCrates: crates }),
 }));
