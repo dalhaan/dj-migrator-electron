997a7e795e4835e35f5990fff6d80d99b5ad98e8
Dallan Freemantle <dallan@alphero.com>
2023-05-26 00:34:51 +1200

    Show import loading indicator and close window on success

--- apps/electron-app/src/renderer/import-window/components/import-button.tsx
+++ apps/electron-app/src/renderer/import-window/components/import-button.tsx
@@ -1,9 +1,11 @@
 import { Crate } from "@dj-migrator/common";
+import { useState } from "react";
 import { Button } from "rsuite";
 
 import { useImport } from "../stores/import-store";
 
 export function ImportButton() {
+  const [isLoading, setIsLoading] = useState(false);
   const directory = useImport((state) => state.directory);
   const crates = useImport((state) => state.crates);
   const selectedCratesPaths = useImport((state) => state.selectedCrates);
@@ -11,15 +13,25 @@ export function ImportButton() {
   async function handleClick() {
     if (!directory) return;
 
+    setIsLoading(true);
+
     const selectedCrates = selectedCratesPaths
       .map((path) => crates.find((crate) => crate.filePath === path))
       .filter((crate) => Boolean(crate)) as Crate[];
 
-    window.electronAPI.parseCrates(directory, selectedCrates);
+    try {
+      await window.electronAPI.parseCrates(directory, selectedCrates);
+
+      // Close window once importing in complete
+      window.close();
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <Button
+      loading={isLoading}
       appearance="primary"
       disabled={!selectedCratesPaths.length}
       onClick={handleClick}
