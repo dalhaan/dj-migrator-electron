7c95d3d9d98371498b26e43f42502b0d4e200f99
Dallan Freemantle <dallan@alphero.com>
2023-05-25 16:29:06 +1200

    Update app container

--- apps/electron-app/src/renderer/components/layout/app-container/app-container.css.ts
+++ apps/electron-app/src/renderer/components/layout/app-container/app-container.css.ts
@@ -8,17 +8,11 @@ export const APP_PADDING = 80;
 export const appContainer = style({
   width: "100%",
   height: "100%",
-  // maxWidth: APP_MAX_WIDTH,
-  marginLeft: "auto",
-  marginRight: "auto",
 
   display: "flex",
   flexDirection: "column",
 
   paddingTop: 20,
-  // paddingRight: APP_PADDING,
-  // paddingBottom: 20,
-  // paddingLeft: APP_PADDING,
 
   overflow: "auto",
 });
