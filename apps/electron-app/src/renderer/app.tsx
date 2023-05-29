aed7ebcb98d8d66f7777967baa740c89bd00ec73
Dallan Freemantle <dallan@alphero.com>
2023-05-29 22:15:34 +1200

    Use dark theme only

--- apps/electron-app/src/renderer/app.tsx
+++ apps/electron-app/src/renderer/app.tsx
@@ -1,24 +1,20 @@
 import clsx from "clsx";
 import { Outlet } from "react-router-dom";
-import { useMedia } from "react-use";
 import { CustomProvider } from "rsuite";
 
 import { AppContainer } from "@/components/layout/app-container";
 import { TitleBar } from "@/components/layout/title-bar";
 import { useWindow } from "@/stores/windowStore";
 import * as styles from "@/styles/app.css";
-import { MEDIA } from "@/styles/media";
 import { darkTheme } from "@/styles/themes/darkTheme.css";
-import { lightTheme } from "@/styles/themes/lightTheme.css";
 
 export const App = () => {
-  const darkMode = useMedia(MEDIA.DARK_MODE);
   const isWindowBlurred = useWindow((state) => !state.isWindowFocused);
 
   return (
-    <CustomProvider theme={darkMode ? "dark" : "light"}>
+    <CustomProvider theme="dark">
       <div
-        className={clsx(darkMode ? darkTheme : lightTheme, styles.app, {
+        className={clsx(darkTheme, styles.app, {
           [styles.windowBlur]: isWindowBlurred,
         })}
       >
