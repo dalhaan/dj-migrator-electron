791b37ad74468057f7c9ae53b009336c00e7681e
Dallan Freemantle <dallan.freemantle@gmail.com>
2023-07-14 15:31:05 +1200

    Remove duplicate code

--- lib/node/src/tracktor-parser/index.ts
+++ lib/node/src/tracktor-parser/index.ts
@@ -4,13 +4,13 @@ import path from "path";
 import musicMetadata from "music-metadata-browser";
 import xml2js from "xml2js";
 
-import CuePoint from "../shared/CuePoint";
 import {
   IMetadata,
   IPlaylist,
   IProgressCallback,
   Tracks,
   Track,
+  CuePoint,
 } from "@dj-migrator/common";
 
 export const SUPPORTED_FILE_TYPES = [".mp3", ".wav", ".flac"];
